e93a08cc9c1f83440521e4e6123e94bf341ff58a
Siphesihle Shezi <138693070+siphesihle1807@users.noreply.github.com>
2025-04-19 23:53:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import smtplib
+import calendar
+import random
+
+# credentials
+
+
+# random quotes
+quotes = open("quotes.txt").read().splitlines()
+quotes = random.choice(quotes)
+print(quotes)
+
+# Date code
+today = dt.datetime.now()
+day_of_week = today.date()
+day = calendar.day_name[day_of_week.weekday()]
+# print(day)
+
+if day=="Saturday":
+    # location of smtp provider. It differs with different email providers.
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+    # secures connection
+        connection.starttls()
+        # login
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+        to_addrs="siphecoded@gmail.com",
+        msg=f'Subject: Saturday Motivation\n\n{quotes}'
+                             )
+
